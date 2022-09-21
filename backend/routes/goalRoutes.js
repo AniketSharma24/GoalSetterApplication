8929d81da24b96f52b0a4a4a7d037f9a83ae67bb
AniketSharma24 <sharmaaniket2411@gmail.com>
2022-09-21 22:27:23 +0530

    Rest API for goals

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const route = express.Router();
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controller/goalsController');
+
+route.route('/').get(getGoals).post(setGoal);
+route.route('/:id').put(updateGoal).delete(deleteGoal);
+
+// route.get('/', getGoals);
+// route.post('/', setGoal);
+// route.put('/:id', updateGoal);
+// route.delete('/:id', deleteGoal);
+
+module.exports = route;
